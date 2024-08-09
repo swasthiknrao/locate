a96600b2a83b81d62b1ba5398b9023501cc0b3f9
swasthiknrao <nraoswasthik2004@gmail.com>
2024-08-09 19:36:35 +0530

    first commit

---
+++ locate.js
@@ -0,0 +1,7 @@
+const clear = document.getElementById("clear");
+const submit = document.getElementById("submit");
+
+
+clearButton.addEventListener('click', () => {
+  document.querySelector("input").value = "";
+});
